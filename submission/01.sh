a817f335a02d16664018e65bc973943ac42c11c3
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-29 11:45:48 +0300

    change a file to test the actions

--- submission/01.sh
+++ submission/01.sh
@@ -1,4 +1,4 @@
 # What is the hash of block 243,833?
-BLOCK_HEeIGHT=243833
-BLOCK_HASH=$(bitcoin-cli -signet getblockhash $BLOCK_HEeIGHT)
+BLOCK_HEIGHT=243833
+BLOCK_HASH=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 getblockhash $BLOCK_HEIGHT)
 printf '%s' "$BLOCK_HASH"
--- submission/02.sh
+++ submission/02.sh
@@ -1,4 +1,4 @@
 # How many new outputs were created by block 243,825?
 BLOCK_HEIGHT=243825
-BLOCK_HASH=$(bitcoin-cli -signet getblockhash $BLOCK_HEIGHT)
+BLOCK_HASH=$(bitcoin-cli -signet rpcuser=btrustbuildersrpc -rpcpassword=btrustbuildersrpcgetblockhash $BLOCK_HEIGHT)
 bitcoin-cli -signet getblock $BLOCK_HASH | jq '.tx | map(.vout | length) | add'
