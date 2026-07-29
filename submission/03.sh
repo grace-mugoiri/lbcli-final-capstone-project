6fa78ac003da4a457995146580ada88052e934a2
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-29 13:08:55 +0300

    rfactor file 3

--- submission/03.sh
+++ submission/03.sh
@@ -1,7 +1,7 @@
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
 PARENT_BLOCK=216128
 PARENT_HASH=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash $PARENT_BLOCK)
-BASE_TXID=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock $PARENT_HASH 2 | jq -r ".tx | .[0] | .txid")
+COINBASE_TX=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock $PARENT_HASH 2 | jq -r ".tx | .[0] | .txid")
 CHILD_BLOCK=216351
 CHILD_HASH=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash $CHILD_BLOCK)
-bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock $CHILD_HASH 2 | jq -r ".tx | .[] | select(.vin[0].txid == \"$BASE_TXID\") | .txid"
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock $CHILD_HASH 2 | jq -r --arg id "$COINBASE_TX" '.tx[] | select(any(.vin[]; .txid == $id)) | .txid'
