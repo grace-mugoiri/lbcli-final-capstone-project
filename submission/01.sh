12e84f23e2c0badb078d87d92ddde24e27ee8fad
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-29 09:17:47 +0300

    write solution 1 and 2

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,4 @@
 # What is the hash of block 243,833?
+BLOCK_HEIGHT=243833
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash $BLOCK_HEIGHT)
+printf '%s' "$BLOCK_HASH"
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # How many new outputs were created by block 243,825?
+BLOCK_HEIGHT=243825
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash $BLOCK_HEIGHT)
+bitcoin-cli -signet getblock $BLOCK_HASH | jq '.tx | map(.vout | length) | add'
