95265e5c6c74ef155a3149acf9772f38272b6af8
Grace Mugoiri <gracemugoiri@gmail.com>
2026-07-29 11:49:31 +0300

    set rpc port

--- submission/01.sh
+++ submission/01.sh
@@ -1,4 +1,4 @@
 # What is the hash of block 243,833?
 BLOCK_HEIGHT=243833
-BLOCK_HASH=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 getblockhash $BLOCK_HEIGHT)
+BLOCK_HASH=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash $BLOCK_HEIGHT)
 printf '%s' "$BLOCK_HASH"
